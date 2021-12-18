1a47e2f8d51bfb0424a7b3be30b85e1680e089b7
Mohsen Rivandi <mohsenrivandy@yahoo.com>
2021-12-19 00:03:08 +0330

    Add project files.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static TcpListener _listener;
+        static List<Client> _users;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+            var client = new Client(_listener.AcceptTcpClient());
+            _users.Add(client);
+        }
+    }
+}
---
+++ MyChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MyChat.MVVM.Core;
+using MyChat.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChat.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServer { get; set; }
+        private Server _server;
+        public string UserName { get; set; }
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServer = new RelayCommand(o => _server.ConnectToServer(UserName), o=>!string.IsNullOrEmpty(UserName));
+        }
+    }
+}
